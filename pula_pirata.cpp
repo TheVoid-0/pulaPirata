09d0b8bf7e6ee2ad7ebf395de48090bc5cea9bce
TheVoid-0 <marco.moura@unoesc.edu.br>
2021-06-03 21:00:01 -0300

    Mudando CMake para alterar o nome do projeto

--- pula_pirata.cpp
+++ pula_pirata.cpp
@@ -15,7 +15,7 @@ GLFWwindow* window;
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
-#include <common/shader.hpp>
+#include <./common/shader.hpp>
 #include <common/texture.hpp>
 #include <common/controls.hpp>
 #include <common/objloader.hpp>
